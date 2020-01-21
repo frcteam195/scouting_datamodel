f3e8b34eb01feac0d6aa71847bb74335c32afd6b
Mark Maciejewski <mark.maciejewski@gmail.com>
2020-01-21 17:58:14 -0500

    fixes to evnets, matches, scorepredictor, and users tables

--- Events-01.sql
+++ Events-01.sql
@@ -1,10 +1,10 @@
 /* Table of Events */
-CREATE TABLE IF NOT EXISTS "Events"
-(
-	"EventID" INT NOT NULL PRIMARY KEY,
-	"EventName" VARCHAR(50) NULL,
-	"EventLocation" VARCHAR(50) NULL,
-	"StartDate" DATETIME NULL,
-	"EndDate" DATETIME NULL,
-	"CurrentEvent" BIT NULL
+
+CREATE TABLE IF NOT EXISTS Events (
+	EventID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	EventName VARCHAR(100) NULL,
+	EventLocation VARCHAR(100) NULL,
+	StartDate DATE NULL,
+	EndDate DATE NULL,
+	CurrentEvent BIT NULL
 );
