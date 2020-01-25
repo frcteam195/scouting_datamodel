297c9ce1ee509de75594bf0f3109be3afeb6dd7c
markmaciejewski <mark.maciejewski@gmail.com>
2020-01-25 14:40:15 -0500

    multi commit

--- MatchScoutingLevelTwo-01.sql
+++ MatchScoutingLevelTwo-01.sql
@@ -2,20 +2,17 @@
 
 CREATE TABLE IF NOT EXISTS Match_Scouting_Level_2
 (
-	MatchScoutingLvl2ID INT NOT NULL PRIMARY KEY,
+	MatchScoutingLvl2ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	EventID INT NOT NULL,
 	MatchID INT NOT NULL,
 	ComputerID INT NOT NULL, 
 	ScouterID INT NOT NULL,
-	ReviewID INT NOT NULL,
-	MatchEnded BOOLEAN NULL,
-	ScoutingStatus INT NULL,
-	AreasToReview INT NULL,
-	Complete BOOLEAN NULL,
+	ReviewerID INT NULL,
+	ScoutingStatus INT DEFAULT 0,
 	TeamRed VARCHAR(10) NULL,
 	TeamBlue VARCHAR(10) NULL,
-	MatchScoutingIDRed INT NULL,
-	MatchScoutingIDBlue INT NULL,
+	MatchScoutingIDRed INT NOT NULL,
+	MatchScoutingIDBlue INT NOT NULL,
 	CommentRed VARCHAR(1040) NULL,
 	CommentBlue VARCHAR(1040) NULL
 );
