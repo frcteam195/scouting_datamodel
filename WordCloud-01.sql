8f4bed76b7ad0fd64d18951360088b08e49992a9
Mark Maciejewski <mark.maciejewski@gmail.com>
2020-01-27 08:38:45 -0500

    change to wordcloud table to add composite primary key

--- WordCloud-01.sql
+++ WordCloud-01.sql
@@ -4,9 +4,8 @@ CREATE TABLE IF NOT EXISTS `team195_scouting`.`WordCloud` (
 	`EventID` INT NOT NULL,
 	`MatchID` INT NOT NULL,
 	`MatchScoutingID` INT NOT NULL,
-	`Seq` INT NULL, 
+	`Seq` INT NOT NULL, 
 	`Team` VARCHAR(10) NULL,
 	`WordID` INT NOT NULL,
-	PRIMARY KEY(`MatchScoutingID`),
-	PRIMARY KEY(`Seq`))
+	PRIMARY KEY(`MatchScoutingID`,`Seq`))
 Engine = InnoDB;
