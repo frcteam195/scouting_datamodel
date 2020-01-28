e311245a5b665bd015dd1330e71cd62181b41992
Diane <fluffyferret33@gmail.com>
2020-01-27 19:16:33 -0500

    Dianes 1st amazin edit

--- Computers-01.sql
+++ Computers-01.sql
@@ -4,7 +4,7 @@
    BOOLEANs in MySQL are TinyInt(1) values
    and hence can be null and can have any single digit
    int inserted. However 0 = False and any other values
-   will be True */
+   will be True  */
 
 CREATE TABLE IF NOT EXISTS `team195_scouting`.`Computers` (
 	`ComputerID` INT AUTO_INCREMENT NOT NULL,
