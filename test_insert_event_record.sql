daf94fdb1584927f82e64918f7be4e86bf838e96
markmaciejewski <mark.maciejewski@gmail.com>
2020-01-30 20:08:02 -0500

    modifying db tables

---
+++ test_insert_event_record.sql
@@ -0,0 +1,14 @@
+INSERT INTO `team195_scouting`.`Events`
+(`EventID`,
+`EventName`,
+`EventLocation`,
+`StartDate`,
+`EndDate`,
+`CurrentEvent`)
+VALUES
+(2,
+"Hartford",
+"Hartford, CT",
+20200117,
+20200119,
+0);
