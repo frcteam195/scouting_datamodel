90585ee48c7a2a096c41e8c42d1b12ab70ceedcf
Mark Maciejewski <mark.maciejewski@gmail.com>
2020-02-03 15:46:34 -0500

    data schema csv files

--- BA-Teams-01.sql
+++ BA-Teams-01.sql
@@ -1,6 +1,6 @@
 /* Admin Tables BA - Teams */
 
-CREATE TABLE IF NOT EXISTS `team195_scouting`.`BA-Teams` (
+CREATE TABLE IF NOT EXISTS `team195_scouting`.`BlueAllianceTeams` (
 	Team VARCHAR(10) NULL,
 	TeamName VARCHAR(50) NULL,
 	TeamLocation VARCHAR(50) NULL,
